a032d262239c2c33edf740b74c9dd2ffd28ec273
Tanvir Mohammad Rizvy <tanvirmohammad2@gmail.com>
2022-02-07 16:21:09 +0600

    updated

--- api/routes/customers.js
+++ api/routes/customers.js
@@ -2,5 +2,21 @@ const router = require("express").Router();
 const Customers = require("../models/Customers");
 
 router.post("/", async(req,res) => {
+    const newCustomer = new Customers(req.body);
+    try{
+        const savedCustomer = await newCustomer.save();
+        res.status(200).json(savedCustomer);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get("/", async(req,res) => {
+    try{
+        const allcustomer = await Customers.find();
+        res.status(200).json(allcustomer);
+    }catch(err){
+        res.status(500).json(err);
+    }
     
-})
+});
